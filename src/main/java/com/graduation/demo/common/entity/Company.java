3aead429fa87bbc53299b1f1b122b185637a1120
zhaoyaoyi <zyy1998001@vip.qq.com>
2020-05-04 02:35:39 +0800

    后端代码V1.0

--- src/main/java/com/graduation/demo/common/entity/Company.java
+++ src/main/java/com/graduation/demo/common/entity/Company.java
@@ -3,19 +3,29 @@ package com.graduation.demo.common.entity;
 import java.io.Serializable;
 
 public class Company implements Serializable {
-    private Integer companyid;
+    private String companyid;
 
     private String contact;
 
     private Integer telephone;
 
+    private String name;
+
+    private String USCC;
+
+    private String address;
+
+    private String registered;
+
+    private String business;
+
     private static final long serialVersionUID = 1L;
 
-    public Integer getCompanyid() {
+    public String getCompanyid() {
         return companyid;
     }
 
-    public void setCompanyid(Integer companyid) {
+    public void setCompanyid(String companyid) {
         this.companyid = companyid;
     }
 
@@ -24,7 +34,7 @@ public class Company implements Serializable {
     }
 
     public void setContact(String contact) {
-        this.contact = contact == null ? null : contact.trim();
+        this.contact = contact;
     }
 
     public Integer getTelephone() {
@@ -33,6 +43,46 @@ public class Company implements Serializable {
 
     public void setTelephone(Integer telephone) {
         this.telephone = telephone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUSCC() {
+        return USCC;
+    }
+
+    public void setUSCC(String USCC) {
+        this.USCC = USCC;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(String registered) {
+        this.registered = registered;
+    }
+
+    public String getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(String business) {
+        this.business = business;
     }
 
     @Override
@@ -44,6 +94,11 @@ public class Company implements Serializable {
         sb.append(", companyid=").append(companyid);
         sb.append(", contact=").append(contact);
         sb.append(", telephone=").append(telephone);
+        sb.append(", name=").append(name);
+        sb.append(", USCC=").append(USCC);
+        sb.append(", address=").append(address);
+        sb.append(", registered=").append(registered);
+        sb.append(", business=").append(business);
         sb.append("]");
         return sb.toString();
     }
